4c40f74738cba4a843515c341dc769e244859c9c
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-05-26 09:06:45 -0400

    starter files

---
+++ index.js
@@ -0,0 +1,25 @@
+// Selectors
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Player Class
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.width = 100;
+    this.height = 100;
+  }
+
+  draw() {
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const player = new Player();
+player.draw();
